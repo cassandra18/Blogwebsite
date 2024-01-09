d6754c06985713ebb5aa9a33e132e8e274d13fe3
cassandra18 <cassandralelei013@gmail.com>
2024-01-09 20:53:15 +0300

    changes made to the users controller file

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,6 +1,15 @@
 const asyncHandler = require('express-async-handler');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
 
 const User = require('../models/userSchema');
+const { hash } = require('bcryptjs');
+const { generateToken } = require('./adminController');
+
+
+const generateToken = (id) => {
+    return (jwt.sign({ id }, process.env.JWT_KEY, { expiresIn: '30min'} ));
+}
 
 const userController =  {
 
@@ -8,21 +17,35 @@ const userController =  {
 
         const { username, email, password } = req.body;
 
+
         if(!username || !email || !password) {
             res.status(400);
             throw new Error('All fields are required!');
         };
 
+
+
+        const userExist = await User.findOne(email);
+
+        if(userExist) {
+            res.status(400);
+            throw new Error('User already exist')
+        }
+        
+        const salt = await bcrypt.genSalt(10);
+
+        const hashedPassword = await bcrypt.hash(password, salt);
+
         const newUser = await User.create({
-            username, email, password
+            username, email, password: hashedPassword
         });
 
-        if(user) {
+        if(newUser && (crypt.compare(password, hashedPassword))) {
             res.status(201).json({
                 _id: newUser.id,
                 username: newUser.username,
                 email: newUser.email,
-                password: newUser.password
+                token: generateToken(newUser.id)
             })
 
 
