512d37b0a21a9a74fc8d58a7c1c2a1ffbf575ec3
cassandra18 <cassandralelei013@gmail.com>
2024-01-12 09:01:28 +0300

    modified the postCOntrollers folder

--- controllers/postController.js
+++ controllers/postController.js
@@ -1,6 +1,37 @@
+const path = require('path');
+const multer = require('multer');
+const { v4: uuidv4 } = require('uuid');
+
 const asyncHandler = require("express-async-handler");
 
 const Post = require("../models/blogPostSchema");
+
+
+
+ // configure the multer storage engine. It specifies that uploaded files should be stored in 'diskStorage' disk. 
+ const storage = multer.diskStorage({
+
+  //set the 'uploads/' directory as the destination directory
+  distination: (req, file, cb) => {
+    cb(null, 'upload/');
+  },
+
+  // Generate a unique filename for each uploaded file. path.extanme() is used to get the extansion of the original file.
+  fileName: (req, file, cb) => {
+    const uniqueFile = `${uuidv4}${path.extname(file.originalName)}`;
+    cb(null, uniqueFile);
+  },
+
+})
+
+
+const uploadMultiple = multer({
+  storage: storage,
+  limits: { files: 5 }, //limits the number of files to be uploaded to 5
+}).array('images', 5) //images is the field name in the form for multiple file
+
+
+
 
 //Define a PostController object
 const postController = {
@@ -22,9 +53,20 @@ const postController = {
       throw new Error("Title already exists");
     }
 
+
+    uploadMultiple(req, res, async (err) => {
+      if (err instanceof multer.MulterError) {
+        res.status(400);
+        throw new Error('Error uploading images');
+      }
+    });
+
+    const imagePaths = req.files.map((file) => `uploads/${file.filename}`);
+
+
     // Create a new blog post
     const newPost = await Post.create({
-        title, content, author, tags
+        title, content, author, tags, imagePaths
     });
 
     if (newPost) {
@@ -34,6 +76,7 @@ const postController = {
         content: newPost.content,
         author: newPost.author,
         tags: newPost.tags,
+        imagePaths: newPost.imagePaths,
       });
 
     } else {
@@ -43,6 +86,18 @@ const postController = {
     
     
   }),
+
+
+
+  //Route handler for handling multiple images
+  uploadMultipleImages: (req, res) => {
+    //Access multiple files through req.file
+    const imagePaths = req.files.map(file => `uploads/${file.filename}`);
+    res.json({ imagePaths })
+  },
+
 };
+
+
 
 module.exports = postController;
--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -2,9 +2,16 @@ const express = require('express');
 const router = express.Router();
 
 const postController = require('../controllers/postController');
+const { authenticateToken } = require('../middleware/authMIddleware');
 
-
+//route for creating post
 router.route('/create-post').post(postController.createPost);
 
+//Route for handling image uploads
+router.route('/upload-image').post(
+    authenticateToken,
+    postController.uploadMultiple,
+    postController.uploadMultipleImages
+    );
 
 module.exports = router;
