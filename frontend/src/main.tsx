42b404d484a47c1751772101981cb21173194aef
Domains18 <gibsonsgibson88@gmail.com>
2024-01-24 11:41:16 +0300

    repository refactor

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,34 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client';
-import App from './App.tsx'
 import './index.css'
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
-import HomePage from './pages/HomePage.tsx';
-import LoginPage from './pages/loginPge.tsx';
+import {RouterProvider}
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App/>,
-    children: [
-      {
-        path: "/home",
-        element: <HomePage />,
-      },
-      {
-        path: "/login",
-        element: <LoginPage />,
-      }
-    ]
-  }
-])
 
 // Render the App component to the Document Object Module
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <RouterProvider router={ router }/>
   </React.StrictMode>,
 )
